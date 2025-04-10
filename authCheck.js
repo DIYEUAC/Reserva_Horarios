5a53eb1524b65fef48c5193bf7e49a23b64ccd06
DIYEUAC <167236622+DIYEUAC@users.noreply.github.com>
2025-04-10 13:32:44 -0500

    Update authCheck.js

--- authCheck.js
+++ authCheck.js
@@ -2,10 +2,10 @@
 
 // Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAe3LW9dme23o1AGXqg4WkJ0",
+  authDomain: "reservas-incuba.firebaseapp.com",
+  projectId: "reservas-incuba",
+  appId: "1:321837960358:web:65127a59a8aa4aae0d7867"
 };
 
 firebase.initializeApp(firebaseConfig);
